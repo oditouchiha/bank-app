a3e376c4a26c1e6ba97e530421d4f31408ca731e
dyaksadito <dyaksa.hanindito@gramedia.id>
2023-10-12 09:31:45 +0700

    feat: :card_file_box: Added forgotten name field to customer model

---
+++ Migrations/20231012023017_AddedNameFieldToCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNameFieldToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Customers");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,4 +9,5 @@ public class Customer : Auditable
     public string PhoneNumber { get; set; } = null!;
     public string Address { get; set; } = null!;
     public Account? Account { get; set; }
+    public string Name { get; set; } = null!;
 }
